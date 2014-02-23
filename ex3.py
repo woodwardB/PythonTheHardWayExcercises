ae4dad9117f467af3cab43b280a9d4e9ce27307c
Brandon Woodward <brandon@brandonwoodward.info>
2014-02-23 15:23:47 -0800

    completed exercise #3

---
+++ ex3.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+# set variable
+print "I will now count my chickens:"
+Hens = 30
+Roosters = 97
+print "Hens = " + str(Hens)
+print "Roosters = " + str(Roosters)
+
+print "Now I will count the eggs:"
+eggs = 7
+print "Eggs = " + str(eggs)
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+if (3+2) < (5-7):
+  print True
+else:
+  print False  
+
+print "What is 3 + 2?"
+print "3 + 2 = " + str((3+2))
+
+print "What is 5 - 7?"
+print "5 -7 = " + str((5-7))
+
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+print "Is it greater?"
+
+if (3+2) > (5-7):
+  print True
+else:
+  print False
+
+print "Is it greater or equal?"
+
+if (3+2) >= (5-7):
+  print True
+else:
+  print False
+
+print "Is it less or equal?"
+if (3+2) <= (5-7):
+  print True
+else:
+  print False
